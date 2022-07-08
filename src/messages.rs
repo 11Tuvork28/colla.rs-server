375bee918f56a477bd7b14c5994ae713c6e48d03
11tuvork28 <zofi@xamh.de>
2022-07-08 15:46:45 +0200

    Added MessageType and ActionType. Implemented  to_string() for both enums

--- src/messages.rs
+++ src/messages.rs
@@ -28,13 +28,55 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Message{
-    pub status_code: i16,
-    pub message: String,
-    pub pet_action_required: Option<String>,
+    status_code: i16,
+    message: String,
+    pet_action_required: Option<String>,
 }
 
 impl Message {
-    pub fn new(status_code: i16, message: String, pet_action_required: Option<String>) -> Message {
-        Message { status_code, message, pet_action_required }
+    pub fn new(status_code: i16, message: MessageTyp, pet_action_required: ActionType) -> Message {
+        Message { status_code, message: message.to_string(), pet_action_required: pet_action_required.to_string() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub enum MessageTyp {
+    ACK,
+    InvalidParams,
+    InvalidCommand,
+    PetOffline,
+    PetUnrecoverableError,
+}
+
+impl MessageTyp {
+    fn to_string(&self) -> String {
+        match *self {
+            MessageTyp::ACK => "ack".to_string(),
+            MessageTyp::InvalidParams =>"Invalid parameters".to_string(),
+            MessageTyp::InvalidCommand => "Invalid command".to_string(),
+            MessageTyp::PetOffline => "Pet went offline".to_string(),
+            MessageTyp::PetUnrecoverableError => "Pet went offline with an unrecoverable error".to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub enum ActionType {
+    Reboot,
+    Reconnect,
+    RebootReconnect,
+    None
+}
+
+impl ActionType {
+    fn to_string(&self) -> Option<String> {
+        match *self {
+            ActionType::Reboot => Some("Request reboot from pet".to_string()),
+            ActionType::Reconnect => Some("Request reconnect from pet".to_string()),
+            ActionType::RebootReconnect => Some("Request reconnect and reboot from pet".to_string()),
+            ActionType::None => None,
+        }
     }
 }
